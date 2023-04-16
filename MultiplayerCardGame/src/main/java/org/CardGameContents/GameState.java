721cf9bcb9fb382d4804be96295761e1203ae7d6
aditya23k <23adikam@gmail.com>
2023-04-16 19:15:58 +0530

    Created a separate play function with a validateMove fn..

---
+++ MultiplayerCardGame/src/main/java/org/CardGameContents/GameState.java
@@ -0,0 +1,33 @@
+package org.CardGameContents;
+
+import java.util.*;
+
+public class GameState {
+    private ArrayList<Players> players;
+    private ArrayList<Card> deck;
+    private Stack<Card> discard;
+    private Stack<Card> draw;
+
+    public GameState(ArrayList<Players> players, ArrayList<Card> deck, Stack<Card> discard, Stack<Card> draw) {
+        this.players = players;
+        this.deck = deck;
+        this.discard = discard;
+        this.draw = draw;
+    }
+
+    public void play(int playerIndex, Card card) {
+        if(!validateMove(card)){
+            throw new UnsupportedOperationException("Illegal move");
+        }
+        players.get(playerIndex).remove(card);
+        discard.push(card);
+    }
+
+    private boolean validateMove(Card card) {
+        // Validate if the move is valid or not.
+        // If the move is valid, return true, else return false.
+        Card discardedCard = discard.peek();
+        return card.getSuite() == discardedCard.getSuite()
+                || card.getVal() == discardedCard.getVal();
+    }
+}
