4a50fda3d0ed0da0c7ef4b4df4ab2662db1a849a
aditya23k <23adikam@gmail.com>
2023-04-17 13:50:54 +0530

    Run the game from this class

--- MultiplayerCardGame/src/main/java/org/CardGameContents/Main.java
+++ MultiplayerCardGame/src/main/java/org/CardGameContents/Main.java
@@ -1,8 +1,22 @@
 package org.CardGameContents;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of players");
+        int numberOfPlayers = sc.nextInt();
+        // Check if number of players is between 2 and 4
+        if (numberOfPlayers < 2 || numberOfPlayers > 4) {
+            System.out.println("Number of players should be between 2 and 4");
+            System.exit(0);
+        }
 
-        System.out.println("Hello world!");
+        // Create a new game
+        GameEngine game = new GameEngine(numberOfPlayers);
+        // Start the game
+        game.playGame();
+
     }
 }
